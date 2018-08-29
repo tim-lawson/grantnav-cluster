ee5e8fe018b4586cfb9c6360b06bc0445f357d92
tslwn <tslawson72@gmail.com>
2018-08-29 13:23:03 +0100

    Towards terms per cluster

---
+++ README.md
@@ -0,0 +1,3 @@
+# Text clustering for grant categorization
+
+Note to self: make sure virtualenv was installed with Python 3!
--- grantnav-cluster.py
+++ grantnav-cluster.py
@@ -1,21 +1,36 @@
 import pandas as pandas
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
 from sklearn.cluster import KMeans
-from sklearn import metrics
 
-# Read data from file
-csv = 'data/grantnav-src-2018.csv'
-dataframe = pandas.read_csv(csv)
+if __name__ == '__main__':
+    # Read data from file
+    csv = 'data/grantnav-src-2018.csv'
+    dataframe = pandas.read_csv(csv)
 
-# Form document from each grant
-dataframe['document'] = dataframe['Title'].map(str) + ' ' + \
-                        dataframe['Description'].map(str)
+    # Form document from each grant
+    dataframe['document'] = dataframe['Title'].map(str) + ' ' + \
+                            dataframe['Description'].map(str)
 
-# Extract features from data set (tokenize, count, weight by TF-IDF)
-tfidf = TfidfVectorizer()
-X = tfidf.fit_transform(dataframe.document)
+    # Extract features from data set (tokenize, count, weight by TF-IDF)
+    tfidf = TfidfVectorizer()
+    X = tfidf.fit_transform(dataframe.document)
 
-# Clustering
-kmeans = KMeans(verbose=True)
-kmeans.fit(X)
-print(metrics.silhouette_score(X, kmeans.labels_))
+    # Clustering
+    km = KMeans(verbose=False)
+    km.fit(X)
+    clusters = km.labels_.tolist()
+
+    # Output file with additional column for assigned cluster
+    dataframe['cluster'] = clusters
+    dataframe.to_csv(path_or_buf='data/grantnav-cluster.csv')
+    # print(dataframe.head())
+
+    # Find terms associated with each cluster by applying TF-IDF to a corpus
+    # of documents that are the concatenation of all grants assigned to each
+    # cluster.
+    cluster_docs = dataframe.groupby('cluster')['document'].agg({
+        'document': lambda x: ' '.join(x)
+    })
+    print(cluster_docs.document[1])
+
+    # TODO: fix aggregation format, run TfidfTransformer...
