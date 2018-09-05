11d42c68b9ab04a0848b4f167d6edfc832223915
tslwn <tslawson72@gmail.com>
2018-09-05 08:48:57 +0100

    (S)K-means and visualisation

--- grantnav-cluster.py
+++ grantnav-cluster.py
@@ -6,21 +6,32 @@ from time import time
 from collections import defaultdict
 from scipy import sparse
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.preprocessing import Normalizer
+from sklearn.pipeline import make_pipeline
+from sklearn.cluster import KMeans
+from spherecluster import SphericalKMeans
+# from sklearn import metrics
+
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 if __name__ == "__main__":
     # Read data from file
+    csv = 'data/grantnav-20180904133708.csv'
     t0 = time()
-    csv = 'data/grantnav-src-2018.csv'
     dataframe = pandas.read_csv(csv)
     print('Read data from file in {:.6f}s'.format(time() - t0))
 
     # Form document from each grant
-    t0 = time()
     dataframe['document'] = dataframe['Title'].map(str) + ' ' + \
                             dataframe['Description'].map(str)
 
     # Form Tf-idf-weighted document-term matrix from corpus
-    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+    t0 = time()
     # This is a sparse matrix of Tf-idf-weightings indexed by (document, term)
     doc_term_matrix = tfidf_vectorizer.fit_transform(dataframe['document'])
     print('Formed Tf-idf-weighted document-term matrix in {:.6f}s'
@@ -28,7 +39,7 @@ if __name__ == "__main__":
 
     # Read pre-trained word embeddings from file
     t0 = time()
-    with open('data\glove.6B.50d.txt', 'rb') as lines:
+    with open('data\glove.6B.100d.txt', 'rb') as lines:
         # Note that map returns an iterator in Python 3
         word2vec = {line.split()[0]: np.array(list(map(float, line.split()[1:])))
                     for line in lines}
@@ -45,11 +56,11 @@ if __name__ == "__main__":
           .format(time() - t0))
 
     # Form document embeddings from average of Tf-idf-weighted word embeddings
-    t0 = time()
     num_docs, num_terms = doc_term_matrix.shape
     doc_embed_matrix = np.zeros((num_docs, dim))
     doc_num_terms = np.zeros(num_docs)
 
+    t0 = time()
     docs, terms = doc_term_matrix.nonzero()
     for doc, term in zip(docs, terms):
         # Inefficient way of calculating the average...
@@ -61,3 +72,33 @@ if __name__ == "__main__":
 
     print('Formed document embedding matrix in {:.6f}s'
           .format(time() - t0))
+
+    # Dimensionality reduction (LSA)
+    t0 = time()
+    svd = TruncatedSVD(n_components=2)
+    normalizer = Normalizer(copy=False)
+    lsa = make_pipeline(svd, normalizer)
+
+    lsa_embed_matrix = lsa.fit_transform(doc_embed_matrix)
+    print('Truncated SVD in {:.6f}s, explained variance {:.3f}'
+          .format(time() - t0), svd.explained_variance_ratio_.sum())
+
+    # Cluster document embeddings
+    kmeans = KMeans(n_clusters=10)
+    # skm = SphericalKMeans(n_clusters=10)
+    t0 = time()
+    kmeans.fit(lsa_embed_matrix)
+    # skm.fit(doc_embed_matrix)
+    print('Clustered document embeddings in {:.6f}s'
+          .format(time() - t0))
+
+    # Visualise clusters in 2-d
+    # pca = PCA(n_components=2).fit_transform(doc_embed_matrix)
+    # tsne = TSNE(n_components=2, verbose=True).fit_transform(doc_embed_matrix)
+
+    plt.figure()
+    plt.scatter(lsa_embed_matrix[:,0], lsa_embed_matrix[:,1])
+    # plt.scatter(pca[:,0], pca[:,1], c=kmeans.labels_.astype(float))
+    # plt.scatter(tsne[:,0], tsne[:,1], c=kmeans.labels_.astype(float))
+    # plt.scatter(tsne[:,0], tsne[:,1], c=skm.labels_.astype(float))
+    plt.show()
