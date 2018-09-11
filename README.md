091866bd8f4aa0add82e3cc9e443fb97e232701e
tslwn <tslawson72@gmail.com>
2018-09-11 07:57:20 +0100

    Top N terms per cluster

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-<<<<<<< HEAD
-If there is no embedding for a word in the corpus, i.e. the word occurs in the corpus we are looking at but not in the corpus used to generate the embeddings, then it is ignored. We might alternatively use an array of zeros or a unit vector with random direction, but neither contributes to the meaning of the subsequent document embedding. If there is no embedding for any of the words in a document, an array of zeros is taken as its embedding.
-
-I suspect it would be simpler to implement the calculation of the embeddings as a sklearn estimator, but I'm not confident enough to do so at the moment.
-=======
 # Text clustering for grant categorization
 
-Note to self: make sure virtualenv was installed with Python 3!
->>>>>>> ee5e8fe018b4586cfb9c6360b06bc0445f357d92
+## Notes
+If there is no embedding for a word in the corpus, i.e. the word occurs in the corpus we are looking at but not in the corpus used to generate the embeddings, then it is ignored. We might alternatively use an array of zeros or a unit vector with random direction, but neither contributes to the meaning of the subsequent document embedding. If there is no embedding for any of the words in a document, an array of zeros is taken as its embedding.
+
+I suspect it would be simpler to implement the calculation of the embeddings as a sklearn estimator, but I'm not confident enough to do so at the moment. (It would be initialised with a path to the GloVe-format txt file and the fit method would include building the dict of embeddings.)
+
+## TODO
+* Apply spherical K-means to document embeddings. Read about von Mises Fisher distribution and especially soft clustering
+* How to evaluate clusters visually (for intuition) and numerically (for hyperparameter search)?
+* Interpretable clusters: save most common terms relative to other clusters (TF-IDF), or the closest word embeddings?
+* Implement whole model as sklearn pipeline - search over number of clusters...
+* Add command line arguments for path to input data...
+* Investigate how the model scales with number of grants, number of clusters...
--- grantnav-cluster.py
+++ grantnav-cluster.py
@@ -1,28 +1,44 @@
 import itertools
 import numpy as np
-import pandas as pandas
+import pandas as pd
 
 from time import time
 from collections import defaultdict
 from scipy import sparse
 from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.cluster import KMeans
+from spherecluster import SphericalKMeans
+
+# https://stackoverflow.com/questions/31790819/scipy-sparse-csr-matrix-how-to-get-top-ten-values-and-indices
+def top_n(row_data, row_indices, n):
+    i = row_data.argsort()[-n:]
+    top_values = row_data[i]
+    top_indices = row_indices[i]
+
+    return top_values, top_indices, i
 
 if __name__ == "__main__":
+    # Arguments
+    input_data = 'data/grantnav-src-2018.csv'
+    output_data = 'data/grantnav-cluster.csv'
+    output_clusters = 'data/clusters.csv'
+    n_clusters = 10
+    n_terms = 10
+
     # Read data from file
     t0 = time()
-    csv = 'data/grantnav-src-2018.csv'
-    dataframe = pandas.read_csv(csv)
+    grants = pd.read_csv(input_data)
     print('Read data from file in {:.6f}s'.format(time() - t0))
 
     # Form document from each grant
     t0 = time()
-    dataframe['document'] = dataframe['Title'].map(str) + ' ' + \
-                            dataframe['Description'].map(str)
+    grants['document'] = grants['Title'].map(str) + ' ' + \
+                         grants['Description'].map(str)
 
     # Form Tf-idf-weighted document-term matrix from corpus
-    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_vectorizer = TfidfVectorizer(stop_words='english')
     # This is a sparse matrix of Tf-idf-weightings indexed by (document, term)
-    doc_term_matrix = tfidf_vectorizer.fit_transform(dataframe['document'])
+    doc_term_matrix = tfidf_vectorizer.fit_transform(grants['document'])
     print('Formed Tf-idf-weighted document-term matrix in {:.6f}s'
           .format(time() - t0))
 
@@ -41,6 +57,7 @@ if __name__ == "__main__":
     t0 = time()
     embed = {v: word2vec.get(k.encode('UTF-8'), np.zeros(dim))
              for k, v in tfidf_vectorizer.vocabulary_.items()}
+
     print('Formed mapping of feature indices to embeddings in {:.6f}s'
           .format(time() - t0))
 
@@ -61,3 +78,54 @@ if __name__ == "__main__":
 
     print('Formed document embedding matrix in {:.6f}s'
           .format(time() - t0))
+
+    # Cluster documents by spherical K-means (cluster centroids are projected
+    # onto the unit hypersphere)
+    t0 = time()
+    skm = SphericalKMeans(n_clusters=n_clusters)
+    skm.fit(doc_embed_matrix)
+    
+    print('Clustered documents by spherical K-means in {:.6f}s'
+          .format(time() - t0))
+
+    # Add column to dataframe to hold assigned cluster and output to CSV
+    grants['cluster'] = skm.labels_.tolist()
+    grants.to_csv(path_or_buf=output_data)
+    
+    # Find terms associated with each cluster by applying Tf-idf to a corpus of
+    # documents that are the concatenation of all grants assigned to each cluster.
+    t0 = time()
+    cluster_docs = grants.groupby('cluster')['document'].agg(lambda x: ' '.join(x))
+    
+    cluster_vectorizer = TfidfVectorizer(stop_words='english')
+    cluster_vectorizer.fit(cluster_docs)
+
+    cluster_term_matrix = cluster_vectorizer.transform(cluster_docs)
+    
+    print('Formed Tf-idf-weighted cluster-term matrix in {:.6f}s'
+          .format(time() - t0))
+
+    # Iterate document-term matrix to retrieve top N terms per cluster
+    t0 = time()
+    features = cluster_vectorizer.get_feature_names()
+
+    cluster_term_matrix_ll = cluster_term_matrix.tolil()
+    cluster_top_n_terms = []
+    for i in range(cluster_term_matrix_ll.shape[0]):
+        data, rows = top_n(np.array(cluster_term_matrix_ll.data[i]),
+                           np.array(cluster_term_matrix_ll.rows[i]),
+                           n_terms)[:2]
+        
+        for feature, weight in zip(rows, data):
+            cluster_top_n_terms.append({
+                'cluster': i,
+                'feature': feature,
+                'word': features[feature],
+                'weight': weight
+            })
+
+    print('Retrieved top {} terms per cluster in {:.6f}s'
+          .format(n_terms, time() - t0))
+
+    clusters = pd.DataFrame(cluster_top_n_terms)
+    clusters.to_csv(path_or_buf=output_clusters)
